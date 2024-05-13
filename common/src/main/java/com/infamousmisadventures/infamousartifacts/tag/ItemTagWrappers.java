975e79271ade5ea2dc0f8295961a59b3ba591580
patrick_provinciael <patrick.provinciael@gmail.com>
2024-05-13 23:57:18 +0200

    Preparing to add tags.

---
+++ common/src/main/java/com/infamousmisadventures/infamousartifacts/tag/ItemTagWrappers.java
@@ -0,0 +1,25 @@
+package com.infamousmisadventures.infamousartifacts.tag;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+import static com.infamousmisadventures.infamousartifacts.Constants.MOD_ID;
+import static com.infamousmisadventures.infamousartifacts.util.ResourceLocationHelper.modLoc;
+
+public class ItemTagWrappers {
+
+    public static final TagKey<Item> ARTIFACT_REPAIR_ITEMS = tag(modLoc("artifact_repair_items"));
+
+    private static TagKey<Item> tag(String name) {
+        return TagKey.create(Registries.ITEM, new ResourceLocation(MOD_ID, name));
+    }
+
+    /*private static TagKey<Item> tag(ResourceLocation resourceLocation) {
+        return TagKey.create(Registry.ITEM_REGISTRY, resourceLocation);
+    }*/
+
+    public static void init() {
+    }
+}
---
+++ common/src/main/java/com/infamousmisadventures/infamousartifacts/util/ResourceLocationHelper.java
@@ -0,0 +1,7 @@
+package com.infamousmisadventures.infamousartifacts.util;
+
+public class ResourceLocationHelper {
+    public static String modLoc(String path) {
+        return "infamousartifacts:" + path;
+    }
+}
